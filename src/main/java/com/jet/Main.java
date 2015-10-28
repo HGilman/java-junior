1dd437477c165f1de591aa476a01e19858de9841
HGilman <hassenbike@gmail.com>
2015-10-28 16:57:21 +0300

    added com.jet.Main and com.present.Presenter

---
+++ src/main/java/com/jet/Main.java
@@ -0,0 +1,8 @@
+package com.jet;
+
+public class Main{
+
+  public static void main(String[] args){
+    com.jet.present.Presenter.print("Hello world");
+  }
+}
---
+++ src/main/java/com/jet/present/Presenter.java
@@ -0,0 +1,9 @@
+package com.jet.present;
+
+public class Presenter{
+  public static void print(String message){
+
+    System.out.println(message);
+  }
+
+}

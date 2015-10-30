64f94b387886ff6cff4830d87cb800699ef22456
HGilman <hassenbike@gmail.com>
2015-10-30 11:25:21 +0300

    Logger changed

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,28 +1,53 @@
 package com.acme.edu;
 
+
+import java.io.*;
+import java.nio.charset.Charset;
+
+/**
+ * This class just prints in console or somewhere else information about
+ * what is happening in whole project
+ */
 public class Logger {
 
 
-
-
-
     public static void log(int message) {
-        printOut("primitive: " + message);
+
+        printToConsole("primitive: ", String.valueOf(message));
     }
 
     public static void log(byte message) {
-        printOut("primitive: " + message);
+        printToConsole("primitive: ", String.valueOf(message));
     }
 
     public static void log(char ch) {
-        printOut("char: " + ch);
+        printToConsole("char: ", String.valueOf(ch));
     }
 
     public static void log(boolean bool) {
-        printOut("primitive: " + bool);
+        printToConsole("primitive: ", String.valueOf(bool));
     }
 
-    private static void printOut(String message){
-        System.out.println(message);
+    /**
+    * Prints message to console
+     */
+    private static void printToConsole(String parameter, String message){
+        System.out.println(parameter + message);
+    }
+
+    /**
+     * Prints message to outPut stream
+     * @param outputStream
+     * @param parameter
+     * @param message
+     */
+    private static void printToOutputStream(OutputStream outputStream, String parameter, String message) {
+
+        String str = parameter + message;
+        try {
+            outputStream.write(str.getBytes(Charset.forName("UTF-8")));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
